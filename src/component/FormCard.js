b1a8356b3a19bd9737a2e0fd83e81e7e20758d96
eletrocp <brernolopes@hotmail.com>
2022-09-19 16:26:13 -0300

    :sparkles: feat: Adicionado form de criação de cartas

--- src/component/FormCard.js
+++ src/component/FormCard.js
@@ -3,7 +3,60 @@ import React from 'react';
 function FormCard() {
   return (
     <form>
-      <p>Form Cards</p>
+      <div id="form-header-container">
+        <input
+          type="checkbox"
+        >
+        </input>
+        <span>
+          <h3>
+            Pontos restantes: 210
+          </h3>
+        </span>
+      </div>
+      <div id="form-name-container">
+        <input 
+          type="text"
+          id="card-name-input"
+          placeholder="Scoot Pilgrim"
+        />
+      </div>
+      <div id="form-atributtes-container">
+        <label>Atributos
+          <input 
+            id="input-attribute-1"
+            type="number"
+            placeholder="Atributo 1"
+          />
+          <input 
+            id="input-attribute-2"
+            type="number"
+            placeholder="Atributo 2"
+          />
+          <input 
+            id="input-attribute-3"
+            type="number"
+            placeholder="Atributo 3"
+          />
+        </label>
+      </div>
+      <div id="form-description-container">
+        <label>
+          Descrição:
+          <textarea
+            id="card-description-input"
+          />
+        </label>
+        <select>
+          <option>Comum</option>
+          <option>Incomum</option>
+          <option>Raro</option>
+        </select>
+      </div>
+      <input
+        type="button"
+        value="Adicionar"
+      />
     </form>
   );
 }
